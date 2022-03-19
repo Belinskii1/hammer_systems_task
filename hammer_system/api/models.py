cc7e4e7b817cd3fbe401ca8b9a09c0f5e1d20041
Belinskii1 <belinskii1@yandex.ru>
2022-03-19 21:38:31 +0300

    admin

--- hammer_system/api/models.py
+++ hammer_system/api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=25,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Псевдоним'
+    )
+    email = models.EmailField(
+        max_length=50,
+        unique=True,
+        verbose_name='Адрес почты'
+    )
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name='Имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name='Фамилия'
+    )
+    telephone_number = models.EmailField(
+        max_length=50,
+        unique=True,
+        verbose_name='номер телефона'
+    )
+
+    USERNAME_FIELD = 'telephone_number'
+    REQUIRED_FIELDS = ('username',)
